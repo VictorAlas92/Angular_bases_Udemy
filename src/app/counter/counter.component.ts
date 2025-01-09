9dd91174af11c10a804caf663018a7fa1c5d59be
VictorAlonsoGLDE348W <victor.2.alonso@continental-corporation.com>
2025-01-09 14:00:52 -0600

    42 contador component aqui cree un componente separado para el counter y puedo ver que al agregar en el module.ts puedo usarlo ahora en el html que usa mi app como si fuera una etiqueta

---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  standalone: false,
+  template: '<h1>Hola Counter</h1>',
+})
+export class CounterComponent {}
