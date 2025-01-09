5f24f2f48890a898c451b1c61b4a766c4620ba8f
VictorAlonsoGLDE348W <victor.2.alonso@continental-corporation.com>
2025-01-09 14:36:45 -0600

    Clase 44 componente Hero y directorios cree nuevos componentes usando la erramienta de CLI con el comando ng n component este crea el componente con HTLM CSS y spec para pruebas y lo agrega en el app.module.ts

---
+++ src/app/heroes/hero/hero.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-hero',
+  standalone: false,
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.css',
+})
+export class HeroComponent {}
