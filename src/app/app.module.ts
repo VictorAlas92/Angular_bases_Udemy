07e09a377577d7a56c98d76644e97b6102b051cf
VictorAlonsoGLDE348W <victor.2.alonso@continental-corporation.com>
2025-01-14 13:08:06 -0600

    Clase 52 tarea sobre modulos se cambian los componentes de list y hero a un sistema gestionado por usu propio modulo y se importa al app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
 import { CounterModule } from './counter/counter.module';
+import { HeroModule } from './heroes/heroes.module';
 
 import { AppComponent } from './app.component';
-import { HeroComponent } from './heroes/hero/hero.component';
-import { ListComponent } from './heroes/list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroComponent,
-    ListComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CounterModule
+    CounterModule,
+    HeroModule
   ],
   providers: [],
   bootstrap: [AppComponent],
