0dc360b489ce13e44c0044e0eadca5543f53ee1c
VictorAlonsoGLDE348W <victor.2.alonso@continental-corporation.com>
2025-01-21 12:58:41 -0600

    Clase 61 pensemos en componentes pequeños transformo el componente de DBZ en componentes mas pequeños

---
+++ src/app/dbz/components/add-character/add-character.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-character',
+  standalone: false,
+  templateUrl: './add-character.component.html',
+  styleUrl: './add-character.component.css',
+})
+export class AddCharacterComponent { }
