ba7440a8c988855c6193af69cd692d0cc455a662
VictorAlonsoGLDE348W <victor.2.alonso@continental-corporation.com>
2025-01-21 14:07:51 -0600

    Clase 63 @input - recibe del padre se manda por medio de un decorador informacion a un componente hijo deste el main page

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-main-page',
@@ -8,4 +9,14 @@ import { Component, OnInit } from '@angular/core';
 
 export class MainPageComponent {
 
+  public character: Character[] = [{
+    name: 'krillin',
+    power: 1000
+  }, {
+    name: 'Goku',
+    power: 9500
+  }, {
+    name: 'Vegueta',
+    power: 7500
+  }];
 }
